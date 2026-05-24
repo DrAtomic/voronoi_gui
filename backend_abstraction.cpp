9c65a1560812f8fbdd94483867beb69b4c643fd5
Leo Garcia <leo.ward.garcia@gmail.com>
2026-05-24 16:27:20 -0700

    breakout plug_reload

--- backend_abstraction.cpp
+++ backend_abstraction.cpp
@@ -15,6 +15,7 @@
 #include "imgui_impl_sdlgpu3.h"
 
 #include "plug.h"
+#include "plug_reload.cpp"
 
 #define VORONOI_SEED_COUNT 40
 #define WINDOW_WIDTH 1280
@@ -41,75 +42,6 @@ static SDL_GPUDevice *gpu_device;
 static SDL_GPUGraphicsPipeline *gpu_pipeline;
 static SDL_GPUTransferBuffer *gpu_transfer_buffer;
 static SDL_GPUBuffer *gpu_buffer;
-
-static const char *lib_plug_name = "./lib_plug.so";
-static void *libplug;
-
-static plug_init_t plug_init;
-static plug_update_t plug_update;
-static plug_pre_reload_t plug_pre_reload;
-static plug_post_reload_t plug_post_reload;
-
-void plug_reload(void)
-{
-	if (libplug)
-		dlclose(libplug);
-
-	for (int attempt = 0; attempt < 10; attempt++) {
-		libplug = dlopen(lib_plug_name, RTLD_NOW | RTLD_GLOBAL);
-		if (libplug)
-			break;
-		usleep(100000);
-	}
-
-	if (!libplug) {
-		fprintf(stderr, "dlopen: %s\n", dlerror());
-		exit(1);
-	}
-
-	plug_init = (plug_init_t)dlsym(libplug, "plug_init");
-	if (!plug_init) {
-		fprintf(stderr, "dlsym plug_init: %s\n", dlerror());
-		exit(1);
-	}
-
-	plug_update = (plug_update_t)dlsym(libplug, "plug_update");
-	if (!plug_update) {
-		fprintf(stderr, "dlsym plug_update: %s\n", dlerror());
-		exit(1);
-	}
-
-	plug_pre_reload = (plug_pre_reload_t)dlsym(libplug, "plug_pre_reload");
-	if (!plug_pre_reload) {
-		fprintf(stderr, "dlsym plug_pre_reload: %s\n", dlerror());
-		exit(1);
-	}
-
-	plug_post_reload = (plug_post_reload_t)dlsym(libplug, "plug_post_reload");
-	if (!plug_post_reload) {
-		fprintf(stderr, "dlsym plug_post_reload: %s\n", dlerror());
-		exit(1);
-	}
-
-	printf("reloading plug\n");
-}
-
-int plug_should_reload(time_t *last_mtime)
-{
-	struct stat st;
-
-	if (stat(lib_plug_name, &st) != 0) {
-		fprintf(stderr, "stat(%s) failed: %s\n", lib_plug_name, strerror(errno));
-		exit(1);
-	}
-
-	if (st.st_mtime != *last_mtime) {
-		*last_mtime = st.st_mtime;
-		return 1;
-	}
-
-	return 0;
-}
 
 static SDL_GPUShader *load_shader_spirv(SDL_GPUDevice *gpu, const char *path, SDL_GPUShaderStage stage, Uint32 num_uniform_buffers, Uint32 num_storage_buffers)
 {
---
+++ plug_reload.cpp
@@ -0,0 +1,68 @@
+static const char *lib_plug_name = "./lib_plug.so";
+static void *libplug;
+
+static plug_init_t plug_init;
+static plug_update_t plug_update;
+static plug_pre_reload_t plug_pre_reload;
+static plug_post_reload_t plug_post_reload;
+
+void plug_reload(void)
+{
+	if (libplug)
+		dlclose(libplug);
+
+	for (int attempt = 0; attempt < 10; attempt++) {
+		libplug = dlopen(lib_plug_name, RTLD_NOW | RTLD_GLOBAL);
+		if (libplug)
+			break;
+		usleep(100000);
+	}
+
+	if (!libplug) {
+		fprintf(stderr, "dlopen: %s\n", dlerror());
+		exit(1);
+	}
+
+	plug_init = (plug_init_t)dlsym(libplug, "plug_init");
+	if (!plug_init) {
+		fprintf(stderr, "dlsym plug_init: %s\n", dlerror());
+		exit(1);
+	}
+
+	plug_update = (plug_update_t)dlsym(libplug, "plug_update");
+	if (!plug_update) {
+		fprintf(stderr, "dlsym plug_update: %s\n", dlerror());
+		exit(1);
+	}
+
+	plug_pre_reload = (plug_pre_reload_t)dlsym(libplug, "plug_pre_reload");
+	if (!plug_pre_reload) {
+		fprintf(stderr, "dlsym plug_pre_reload: %s\n", dlerror());
+		exit(1);
+	}
+
+	plug_post_reload = (plug_post_reload_t)dlsym(libplug, "plug_post_reload");
+	if (!plug_post_reload) {
+		fprintf(stderr, "dlsym plug_post_reload: %s\n", dlerror());
+		exit(1);
+	}
+
+	printf("reloading plug\n");
+}
+
+int plug_should_reload(time_t *last_mtime)
+{
+	struct stat st;
+
+	if (stat(lib_plug_name, &st) != 0) {
+		fprintf(stderr, "stat(%s) failed: %s\n", lib_plug_name, strerror(errno));
+		exit(1);
+	}
+
+	if (st.st_mtime != *last_mtime) {
+		*last_mtime = st.st_mtime;
+		return 1;
+	}
+
+	return 0;
+}
